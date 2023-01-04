e1477f6cd15cd407e2930d37fc1157fcffa7bbdc
Nasikh CL <nasikcl@gmail.com>
2023-01-04 19:50:06 +0530

    content updated in user details component

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -5,9 +5,9 @@ function UserDetails() {
 
   return (
     <div className='user-details-card'>
-    <h3>Demo Booking Already scheduled.</h3>
+    <h3>Demo Booking scheduled.</h3>
     <i style={{color:"#4BB543", fontSize:"100px",margin:"20px 0px"}} className="fa-regular fa-circle-check"></i>
-       <p>we have already sent you the details through email </p> 
+       <p>we have  sent you the details through email </p> 
     <p>Check your Email: {UserDetails.email}</p>
     <p>Thank you for booking The demo  Voosh!</p>
     </div>
