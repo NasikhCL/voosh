a49b640a0133318ce5a82e64b2b2f0f421eedf7b
Nasikh CL <nasikcl@gmail.com>
2023-01-04 19:47:43 +0530

    added brands-logo in landing page

--- src/components/BrandLogos.js
+++ src/components/BrandLogos.js
@@ -3,12 +3,12 @@ import './brands-logos.css'
 const BrandLogos = () => {
   return (
     <div class="brand-logos">
-            <img src="https://voosh.in/static/media/page1_1.cb1dbedcdc321b25fb4e.webp" className="page1-outlet-icons" alt='brandImg' />
-            <img src="https://voosh.in/static/media/page1_1.cb1dbedcdc321b25fb4e.webp" className="page1-outlet-icons"  alt='brandImg' />
-            <img src="https://voosh.in/static/media/page1_1.cb1dbedcdc321b25fb4e.webp" className="page1-outlet-icons"  alt='brandImg' />
-            <img src="https://voosh.in/static/media/page1_1.cb1dbedcdc321b25fb4e.webp" className="page1-outlet-icons"  alt='brandImg' />
-            <img src="https://voosh.in/static/media/page1_1.cb1dbedcdc321b25fb4e.webp" className="page1-outlet-icons"  alt='brandImg' />
-            <img src="https://voosh.in/static/media/page1_1.cb1dbedcdc321b25fb4e.webp" className="page1-outlet-icons"  alt='brandImg' />
+            <img src="https://voosh.in/static/media/page1_5.f505a37d3e86d1a571da.webp" className="page1-outlet-icons page1-outlet-icon1" alt='brandImg' />
+            <img src="https://voosh.in/static/media/page1_2.4bfc2d124bbd2e50da2d.webp" className="page1-outlet-icons page1-outlet-icon2"  alt='brandImg' />
+            <img src="https://voosh.in/static/media/page1_3.1d108925b5e5156bb2d4.webp" className="page1-outlet-icons page1-outlet-icon3"  alt='brandImg' />
+            <img src="https://voosh.in/static/media/page1_1.cb1dbedcdc321b25fb4e.webp" className="page1-outlet-icons page1-outlet-icon4"  alt='brandImg' />
+            <img src="https://voosh.in/static/media/page1_6.2213ad7f35bd24758114.webp" className="page1-outlet-icons page1-outlet-icon5"  alt='brandImg' />
+            <img src="https://voosh.in/static/media/page1_4.a2a22e7f5e42a50552ae.webp" className="page1-outlet-icons page1-outlet-icon6"  alt='brandImg' />
           </div>
   )
 }
